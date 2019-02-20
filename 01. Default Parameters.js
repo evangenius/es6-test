e0ad4a9b62a1b9ef5d92627aea02d53971722ea0
evan- <evangenius@hotmail.com>
2019-02-21 05:10:28 +0800

    add 01

---
+++ 01. Default Parameters.js
@@ -0,0 +1,13 @@
+var link = function(
+    height = 50,
+    color = 'red',
+    url = 'http://azat.co'
+) {
+    console.log(height, color, url);
+}
+console.log(1)
+link;
+console.log(2)
+link();
+console.log(3)
+link(111, 222, 333);
